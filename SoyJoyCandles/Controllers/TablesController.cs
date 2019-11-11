e897ab741314a18d2ae8c166bdf91efed2cec882
TP041249 <TP041249@mail.apu.edu.my>
2019-11-11 22:43:05 +0800

    commit6

--- SoyJoyCandles/Controllers/TablesController.cs
+++ SoyJoyCandles/Controllers/TablesController.cs
@@ -8,12 +8,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage.Auth;
+using SoyJoyCandles.Models;
 
 namespace SoyJoyCandles.Controllers
 {
     public class TablesController : Controller
     {
-        public IActionResult Index()
+
+		public IActionResult Index()
         {
             return View();
         }
@@ -25,13 +27,47 @@ namespace SoyJoyCandles.Controllers
 				.AddJsonFile("appsettings.json");
 			IConfigurationRoot Configuration = builder.Build();
 			CloudStorageAccount storageAccount =
-				CloudStorageAccount.Parse(Configuration["ConnectionStrings:AzureStorageConnectionString-1"]);
+				CloudStorageAccount.Parse(Configuration["ConnectionStrings:DefaultEndpointsProtocol=https;AccountName=soyjoystorageaspnet;AccountKey=mK17ti5JHcBLNFNxsdmVhayIPqjLwJBTHaQMIsGLiGIvBn49nLVn8Nm6VJDxXAibTHuKD/yFio7zP03oazTjTQ==;EndpointSuffix=core.windows.net"]);
 
 			CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-			CloudTable table = tableClient.GetTableReference("TestTable");
+			CloudTable table = tableClient.GetTableReference("CandleTable");
 			ViewBag.Success = table.CreateIfNotExistsAsync().Result;
 			ViewBag.TableName = table.Name;
 			return View();
+		}
+
+		public ActionResult AddEntity()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+			IConfigurationRoot Configuration = builder.Build();
+			CloudStorageAccount storageAccount =
+				CloudStorageAccount.Parse(Configuration["ConnectionStrings:DefaultEndpointsProtocol=https;AccountName=soyjoystorageaspnet;AccountKey=mK17ti5JHcBLNFNxsdmVhayIPqjLwJBTHaQMIsGLiGIvBn49nLVn8Nm6VJDxXAibTHuKD/yFio7zP03oazTjTQ==;EndpointSuffix=core.windows.net"]);
+
+			CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+			CloudTable table = tableClient.GetTableReference("CandleTable");
+
+			CandleEntity candle1 = new CandleEntity("African", "Musk");
+			candle1.CandleProducedDate = "21/09/2019";
+			candle1.CandleType = "Musky";
+			candle1.Candleprice = "95.40";
+
+			TableOperation insertOperation = TableOperation.Insert(candle1);
+
+			try
+			{
+				TableResult results = table.ExecuteAsync(insertOperation).Result;
+				ViewBag.TableName = table.Name;
+				ViewBag.result = results.HttpStatusCode;
+			}
+			catch (Exception ex)
+			{
+				ViewBag.result = 101; //error page
+			}
+			
+			return View();
+
 		}
 
     }
---
+++ SoyJoyCandles/Models/CandleEntity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace SoyJoyCandles.Models
+{
+	public class CandleEntity : TableEntity
+	{
+		public CandleEntity(string originName, string typeName)
+		{
+			this.PartitionKey = originName; //unique identifier
+			this.RowKey = typeName;
+		}
+
+		public CandleEntity() { }
+
+		//public string Email { get; set; }
+		//public int ID { get; set; }
+
+		//[StringLength(60, MinimumLength = 3)]
+		//[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+		//[Display(Name = "Name of Candle")]
+		//[Required]
+		//public string CandleName { get; set; }
+
+		//[Display(Name = "Produced Date")]
+		//[DataType(DataType.Date)]
+		//[Required]
+		public string CandleProducedDate { get; set; }
+		//if you dont want date time but just want date, you can use DataType(DataType.Date)
+
+		//[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+		//[Display(Name = "Type of Candle")]
+		//[Required]
+		public string CandleType { get; set; }
+
+		//[Display(Name = "Price of Candle")]
+		//[DataType(DataType.Currency)] //set currency
+		//[Range(1, 200)] //until 2hundred, cannot add more
+		//[Required]
+		public string Candleprice { get; set; }
+	}
+}
