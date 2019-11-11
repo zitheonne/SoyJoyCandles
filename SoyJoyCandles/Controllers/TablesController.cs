cc38ffde076c2f8d806b112b2f8e2ee85d40c2da
TP041249 <TP041249@mail.apu.edu.my>
2019-11-11 21:41:26 +0800

    commit5

---
+++ SoyJoyCandles/Controllers/TablesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace SoyJoyCandles.Controllers
+{
+    public class TablesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult CreateTable()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+			IConfigurationRoot Configuration = builder.Build();
+			CloudStorageAccount storageAccount =
+				CloudStorageAccount.Parse(Configuration["ConnectionStrings:AzureStorageConnectionString-1"]);
+
+			CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+			CloudTable table = tableClient.GetTableReference("TestTable");
+			ViewBag.Success = table.CreateIfNotExistsAsync().Result;
+			ViewBag.TableName = table.Name;
+			return View();
+		}
+
+    }
+}
