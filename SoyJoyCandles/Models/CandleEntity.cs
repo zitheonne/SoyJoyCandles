7fedd0b61df697cf54adf9cf6c118b73324076e4
TP041249 <TP041249@mail.apu.edu.my>
2019-11-25 01:19:36 +0800

    commitlast

--- SoyJoyCandles/Models/CandleEntity.cs
+++ SoyJoyCandles/Models/CandleEntity.cs
@@ -16,31 +16,11 @@ namespace SoyJoyCandles.Models
 		}
 
 		public CandleEntity() { }
-
-		//public string Email { get; set; }
-		//public int ID { get; set; }
-
-		//[StringLength(60, MinimumLength = 3)]
-		//[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-		//[Display(Name = "Name of Candle")]
-		//[Required]
-		//public string CandleName { get; set; }
-
-		//[Display(Name = "Produced Date")]
-		//[DataType(DataType.Date)]
-		//[Required]
+		
 		public string CandleProducedDate { get; set; }
-		//if you dont want date time but just want date, you can use DataType(DataType.Date)
 
-		//[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-		//[Display(Name = "Type of Candle")]
-		//[Required]
 		public string CandleType { get; set; }
 
-		//[Display(Name = "Price of Candle")]
-		//[DataType(DataType.Currency)] //set currency
-		//[Range(1, 200)] //until 2hundred, cannot add more
-		//[Required]
 		public string Candleprice { get; set; }
 
 		[Key]
